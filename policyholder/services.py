0bfa5a5b79609c33d4b16d979c587891dbca40d7
sniedzielski <sniedzielski@soldevelo.com>
2020-12-11 15:20:20 +0100

    Revert "OFS-98: starting working on create/update PolicyHolder's services"
    This reverts commit 9ca16912e4502bcb3e5341c6a5a9d2c062a3ad52.

--- policyholder/services.py
+++ policyholder/services.py
@@ -1,78 +1,34 @@
-import core
-
+import xml.etree.ElementTree as ET
 from django.core.exceptions import PermissionDenied
+import core
 from django.db import connection, transaction
-from django.contrib.auth.models import AnonymousUser
-from django.core.exceptions import ValidationError
+
 from django.conf import settings
-from django.core import serializers
-
-from policyholder.models import PolicyHolder as PolicyHolderModel, PolicyHolderUser as PolicyHolderUserModel, \
-    PolicyHolderContributionPlan as PolicyHolderContributionPlanModel, PolicyHolderInsuree as PolicyHolderInsureeManager
 
 
+@core.comparable
 class PolicyHolder(object):
 
-    def __init__(self, user):
-        self.user = user
-
-    def get(self, policy_holder):
+    def __init__(self, policy_holder):
+        self.policy_holder = policy_holder
         pass
 
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
     def create(self, policy_holder):
-        if type(self.user) is AnonymousUser or not self.user.id:
-            return {
-                "success": False,
-                "message": "Authentication required",
-                "detail": "PermissionDenied",
-            }
-        try:
-            phm = PolicyHolderModel(**policy_holder)
-            phm.save(username=self.user.username)
-            return {
-                "success": True,
-                "message": "Ok",
-                "detail": serializers.serialize("json", phm),
-                "data": "",
-            }
-        except Exception as exc:
-            return{
-                "success": False,
-                "message": "Failed to create PolicyHolder",
-                "detail": str(exc),
-                "data": "",
-            }
+        pass
 
     def update(self, policy_holder):
-        if type(self.user) is AnonymousUser or not self.user.id:
-            return {
-                "success": False,
-                "message": "Authentication required",
-                "detail": "PermissionDenied",
-                "data": "",
-            }
-        try:
-            updated_phm = PolicyHolderModel.objects.filter(id=policy_holder['id']).first()
-            [setattr(updated_phm, key, policy_holder[key]) for key in policy_holder]
-            updated_phm.save(username=self.user.username)
-            return {
-                "success": True,
-                "message": "Ok",
-                "detail": "",
-                "data": serializers.serialize("json", updated_phm),
-            }
-        except Exception as exc:
-            return{
-                "success": False,
-                "message": "Failed to create PolicyHolder",
-                "detail": str(exc),
-                "data": "",
-            }
+        pass
 
     def delete(self, policy_holder):
         pass
 
     def replace_policy_holder(self, policy_holder):
+        pass
+
+    def remove_policy_holder(self, policy_holder):
         pass
 
 
