fa4fe079ae2b845b48568b71b5ce80404b91a36c
sniedzielski <sniedzielski@soldevelo.com>
2021-03-31 10:08:58 +0200

    change the key word for ph uuid/id

--- policyholder/signals.py
+++ policyholder/signals.py
@@ -11,11 +11,11 @@ def append_policy_holder_filter(sender, **kwargs):
     filters = kwargs.get('filters', None)
     filter_ph = None
     if user and filters:
-        if "policyholder_uuid" in additional_filter:
+        if "policyHolder" in additional_filter:
             # then check perms
             # TODO add in if statement the checking for portal payment perms
             if user.has_perms(PaymentConfig.gql_query_payments_perms):
-                ph_id = additional_filter["policyholder_uuid"]
+                ph_id = additional_filter["policyHolder"]
                 filter_ph = Q(payment_details__premium__contract_contribution_plan_details__contract_details__contract__policy_holder__id=ph_id)
     if filter_ph:
         filters.append(filter_ph)
