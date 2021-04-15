86d1488548a49301bd82433389dfa79db7c66e2a
sniedzielski <sniedzielski@soldevelo.com>
2021-04-15 14:45:40 +0200

    OFS-279: hothix filter PHUser

--- policyholder/signals.py
+++ policyholder/signals.py
@@ -11,24 +11,14 @@ def append_policy_holder_filter(sender, **kwargs):
     additional_filter = kwargs.get('additional_filter', None)
     if "policyHolder" in additional_filter:
         # then check perms
-        print("before checking payment perms")
         if user.has_perms(PaymentConfig.gql_query_payments_perms) or user.has_perms(PolicyholderConfig.gql_query_payment_portal_perms):
-            print("perms ok")
             ph_id = additional_filter["policyHolder"]
             # check if user is linked to ph in policy holder user table
             type_user = f"{user}"
             # related to user object output (i) or (t)
             # check if we have interactive user from current context
-            print(type_user)
-            print("check type user")
             if '(i)' in type_user:
-                print("yes, interactive user")
-                print(user.i_user.id)
-                print(user.i_user)
-                print(user.uuid)
                 ph_user = PolicyHolderUser.objects.filter(Q(policy_holder__id=ph_id, user__id=user.i_user.id)).first()
-                print("check ph user exist")
-                print(ph_user)
                 if ph_user:
                     return Q(
                         payment_details__premium__contract_contribution_plan_details__contract_details__contract__policy_holder__id=ph_id
